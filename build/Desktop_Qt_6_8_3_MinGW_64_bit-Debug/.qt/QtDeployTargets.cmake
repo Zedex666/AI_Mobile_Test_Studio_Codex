58330208b355187ecf730438606a04df593c92fc
Stream <3267435005@qq.com>
2026-07-25 14:28:35 +0800

    v0.0.2

--- build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_AI_Mobile_Test_Studio_Codex_FILE E:/Project/AI_Mobile_Test_Studio_Codex/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/AI_Mobile_Test_Studio_Codex.exe)
-set(__QT_DEPLOY_TARGET_AI_Mobile_Test_Studio_Codex_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_AI_Mobile_Test_Studio_Codex_RUNTIME_DLLS D:/myApp_666666666666666/QT/app/6.8.3/mingw_64/bin/Qt6Widgets.dll;D:/myApp_666666666666666/QT/app/6.8.3/mingw_64/bin/Qt6Gui.dll;D:/myApp_666666666666666/QT/app/6.8.3/mingw_64/bin/Qt6Core.dll)
